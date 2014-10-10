ce86792cfcf414547926ad8d6fbd6fc7c1619c28
Spencer Zhang <syz4@cornell.edu>
2014-10-10 10:29:44 -0500

    Set up mock login interface

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,35 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var app = angular.module("TodoApp", [])
+
+var API_ROOT = ""
+
+app.run(function($rootScope) {
+  $rootScope.user = {loggedIn: false}
+})
+
+function ErrorController($scope, $rootScope) {
+
+}
+
+function SessionController($scope, $rootScope) {
+  var self = this;
+
+  this.action = 'Log In'
+  this.user = $rootScope.user
+
+  this.login = function() {
+    $rootScope.user.loggedIn = true;
+  }
+
+  this.logout = function() {
+    $rootScope.user.loggedIn = false;
+  }
+}
+
+function TodoController($scope, $rootScope) {
+  var self = this;
+  this.user = $rootScope.user
+  this.todos = []
+}
