74571c9521fb1ae59860473aec1f772de92eb030
Spencer Zhang <syz4@cornell.edu>
2014-10-10 11:00:22 -0500

    Implemented error messages when signing in

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,14 +3,14 @@
 
 var app = angular.module("TodoApp", [])
 
-var API_ROOT = ""
+var API_ROOT = "http://recruiting-api.nextcapital.com/"
 
 app.run(function($rootScope) {
   $rootScope.user = {loggedIn: false}
 })
 
 function ErrorController($scope, $rootScope) {
-
+  this.message = $rootScope.error;
 }
 
 function SessionController($scope, $rootScope) {
@@ -18,9 +18,23 @@ function SessionController($scope, $rootScope) {
 
   this.action = 'Log In'
   this.user = $rootScope.user
+  this.error = ""
+  $scope.form = {email: "test", password: ""}
 
   this.login = function() {
-    $rootScope.user.loggedIn = true;
+    $.ajax({
+      url: API_ROOT + "users/sign_in",
+      type: "POST",
+      data: $scope.form,
+      success: function(body) {
+        console.log(body)
+      },
+      error: function(body) {
+        var errorMessage = JSON.parse(body.responseText)['error'];
+        self.error = errorMessage;
+        $scope.$apply()
+      }
+    })
   }
 
   this.logout = function() {
@@ -32,4 +46,10 @@ function TodoController($scope, $rootScope) {
   var self = this;
   this.user = $rootScope.user
   this.todos = []
+
+  $scope.$watch("user.loggedIn", function() {
+    if(self.user.loggedIn) {
+
+    }
+  })
 }
