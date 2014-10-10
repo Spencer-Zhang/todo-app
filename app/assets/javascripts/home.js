7388c7cdbddbfc2c2983d1896c6ad7837d6b6314
Spencer Zhang <syz4@cornell.edu>
2014-10-10 12:55:29 -0500

    Implemented adding new tasks and loading existing tasks

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,60 +6,87 @@ var app = angular.module("TodoApp", [])
 var API_ROOT = "http://recruiting-api.nextcapital.com/"
 
 app.run(function($rootScope) {
-  $rootScope.user = {user_id: "", api_token: ""}
+  $rootScope.user = {}
 })
 
-function ErrorController($scope, $rootScope) {
-  this.message = $rootScope.error;
-}
-
-function SessionController($scope, $rootScope) {
+function SessionController($scope, $rootScope, $http) {
   var self = this;
 
   this.action = 'Log In'
-  this.user = $rootScope.user
+
   this.error = ""
-  $scope.form = {email: "test", password: ""}
+  $scope.form = {email: "testuser", password: "test"}
+
+  this.loggedIn = false;
 
   this.login = function() {
     var path;
     if($scope.form.email != "" && $scope.form.password != "") {
-      if(self.action == "Log In") {
-        path = "users/sign_in"
-      } else if (self.action == "Sign Up") {}
-        path = "users"
-      }
-      $.ajax({
-        url: API_ROOT + path,
-        type: "POST",
-        data: $scope.form,
-        success: function(body) {
-          $rootScope.user = JSON.parse(body);
-        },
-        error: function(body) {
-          var errorMessage = JSON.parse(body.responseText);
-          self.error = errorMessage;
-          $scope.$apply()
-        }
+      if(self.action == "Log In") { path = "users/sign_in" }
+      if(self.action == "Sign Up") { path = "users" }
+
+      var responsePromise = $http.post(API_ROOT + path, $scope.form)
+      responsePromise.success(function(data) {
+        $rootScope.user = data;
+        self.loggedIn = true;
+      })
+      responsePromise.error(function(data) {
+        self.error = data;
       })
     } else {
-      self.error = "Fields cannot be blank"
+      self.error = "Fields cannot be blank";
     }
   }
 
   this.logout = function() {
-    $rootScope.user.loggedIn = false;
+    self.loggedIn = false;
+    $rootScope.user = {};
+    $http.delete(API_ROOT + "users/sign_out", $rootScope.user)
   }
 }
 
-function TodoController($scope, $rootScope) {
+function TodoController($scope, $rootScope, $http) {
   var self = this;
-  this.user = $rootScope.user
-  this.todos = []
+  var user_path = ""
+  var user_key = {}
+  this.error = ""
 
-  $scope.$watch("user.loggedIn", function() {
-    if(self.user.loggedIn) {
+  $rootScope.$watch("user", function() {
+    self.user = $rootScope.user
 
+    console.log(self.user)
+
+    if(self.user.id != undefined) {
+      user_path = API_ROOT + "users/" + self.user.id
+      user_key = {
+        user_id: self.user.id.toString(),
+        api_token: self.user.api_token
+      }
+
+      responsePromise = $http.get(user_path + "/todos", {params: user_key})
+      responsePromise.success(function(data) {
+        console.log(data)
+        self.todos = data
+      })
+      responsePromise.error(function(data) {console.log(data)})
     }
   })
+
+  this.createTodo = function() {
+    var responsePromise = $http.post(user_path + "/todos", user_key)
+
+    responsePromise.success(function(data) {
+      self.todos.push(data);
+    })
+
+    responsePromise.error(function(data) {
+      self.error = data;
+    })
+  }
+
+  this.save = function() {
+    $.each(self.todos, function(index, user_key) {
+      console.log(todo)
+    })
+  }
 }
