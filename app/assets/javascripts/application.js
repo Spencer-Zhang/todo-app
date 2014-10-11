ea9dcecb2c512c6433eac5e4f85dfa1c08cadb49
Spencer Zhang <syz4@cornell.edu>
2014-10-11 11:18:05 -0500

    Merged home.js with application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,33 @@
 //= require turbolinks
 //= require angular
 //= require_tree .
+
+
+var app = angular.module("TodoApp", [])
+
+var API_ROOT = "http://recruiting-api.nextcapital.com/"
+
+app.run(function($rootScope) {
+  $rootScope.user = {}
+})
+
+app.directive('contenteditable', ['$sce', function($sce) {
+    return {
+      restrict: 'A', // only activate on element attribute
+      require: '?ngModel', // get a hold of NgModelController
+
+      link: function(scope, element, attrs, ngModel) {
+        function read() {
+          ngModel.$setViewValue(element.html());
+        }
+
+        ngModel.$render = function() {
+          element.html(ngModel.$viewValue || "");
+        };
+
+        element.bind("blur keyup change", function() {
+          scope.$apply(read);
+        });
+      }
+    };
+  }]);
